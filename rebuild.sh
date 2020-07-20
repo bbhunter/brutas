f8939148275a2c62127720e3b094080c65f7effb
tasooshi <tasooshi@pm.me>
2020-07-21 00:43:13 +0200

    Updated usernames, build fix

--- rebuild.sh
+++ rebuild.sh
@@ -12,7 +12,7 @@ hashcat -r ./brutas-passwords-hax0r.rule --stdout ./brutas-passwords-2.txt | sor
 hashcat -r ./brutas-passwords-years.rule --stdout ./brutas-usernames.txt > brutas-passwords-usernames-years.txt
 hashcat -r ./brutas-passwords-hax0r.rule --stdout ./brutas-usernames.txt > brutas-passwords-usernames-hax0r.txt
 cat brutas-passwords-usernames-years.txt \
-    brutas-passwords-usernames-hax0r.txt | sort -f  | uniq >> brutas-passwords-3.txt
+    brutas-passwords-usernames-hax0r.txt | sort -f  | uniq > brutas-passwords-3.txt
 rm brutas-passwords-usernames-*
 cat brutas-passwords-1-hax0r.txt \
     brutas-passwords-1-years.txt \
