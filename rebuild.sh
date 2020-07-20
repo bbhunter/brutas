bb52737a0ff2dbf6512b1e89dcf05c18ae48ac3c
tasooshi <tasooshi@pm.me>
2020-07-20 22:53:59 +0200

    Just a regular update and improvements

--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+sort -f brutas-passwords-1.txt > sorted1
+sort -f brutas-passwords-2.txt > sorted2
+comm -13 sorted1 sorted2 > brutas-passwords-2.txt
+rm brutas-passwords-1.txt sorted2
+mv sorted1 brutas-passwords-1.txt
 hashcat -r ./brutas-passwords-years.rule --stdout ./brutas-passwords-1.txt > brutas-passwords-1-years.txt
 hashcat -r ./brutas-passwords-years.rule --stdout ./brutas-passwords-2.txt > brutas-passwords-2-years.txt
-hashcat -r ./brutas-passwords-hax0r.rule --stdout ./brutas-passwords-1.txt | sort | uniq > brutas-passwords-1-hax0r.txt
-hashcat -r ./brutas-passwords-hax0r.rule --stdout ./brutas-passwords-2.txt | sort | uniq > brutas-passwords-2-hax0r.txt
+hashcat -r ./brutas-passwords-hax0r.rule --stdout ./brutas-passwords-1.txt | sort -f | uniq > brutas-passwords-1-hax0r.txt
+hashcat -r ./brutas-passwords-hax0r.rule --stdout ./brutas-passwords-2.txt | sort -f | uniq > brutas-passwords-2-hax0r.txt
 hashcat -r ./brutas-passwords-years.rule --stdout ./brutas-usernames.txt > brutas-passwords-usernames-years.txt
 hashcat -r ./brutas-passwords-hax0r.rule --stdout ./brutas-usernames.txt > brutas-passwords-usernames-hax0r.txt
 cat brutas-passwords-usernames-years.txt \
-    brutas-passwords-usernames-hax0r.txt | sort | uniq >> brutas-passwords-3.txt
+    brutas-passwords-usernames-hax0r.txt | sort -f  | uniq >> brutas-passwords-3.txt
 rm brutas-passwords-usernames-*
 cat brutas-passwords-1-hax0r.txt \
     brutas-passwords-1-years.txt \
@@ -15,4 +20,4 @@ cat brutas-passwords-1-hax0r.txt \
     brutas-passwords-2-hax0r.txt \
     brutas-passwords-2-years.txt \
     brutas-passwords-2.txt \
-    brutas-passwords-3.txt | sort | uniq > brutas-passwords-combined.txt
+    brutas-passwords-3.txt | sort -f  | uniq > brutas-passwords-combined.txt
