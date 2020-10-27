0d59d373389a13b14bdecd5cc3185b6a817f2690
tasooshi <tasooshi@pm.me>
2020-10-27 20:50:35 +0100

    General housekeeping, added other languages, wifi keywords, HTTP params and some new combinations

--- README.md
+++ README.md
@@ -2,11 +2,7 @@
 
     ░▒▓ brutas ▓▒░ Wordlists and passwords handcrafted with ♥
 
-## Descriptions
-
-These dictionaries are based on real-life pentesting engagements both in small business and corporate network environments, leaks monitoring, honeypots, some friendly wargames etc. The build process is automated and handled by the script located in `./bin/rebuild.sh`. Check it out to understand what are the blocks and how I set the priorities (or in other words what is most probable in my opinion).
-
-### Passwords
+## Passwords
 
 The `brutas-passwords-10k.txt` list seems to be most effective for general purpose and reasonably fast password bruteforcing, while `brutas-passwords-1k.txt` is designed for a quick win in large networks. Somewhat balanced version of these two is `brutas-passwords-5k.txt` which is perfect for targeting local network services and admin accounts thanks to close keys combinations.
 
@@ -19,23 +15,33 @@ However, I recommend experimenting on your own and rebuilding these sets dependi
 * `brutas-passwords-400k.txt`
 * `brutas-passwords-classics.txt`
 * `brutas-passwords-closekeys.txt`
-* `brutas-passwords-keywords-en-less-common.txt`
-* `brutas-passwords-keywords-en-more-common.txt`
 * `brutas-passwords-numbers.txt`
 * `brutas-passwords-tomcat.txt`
 * `brutas-passwords-unique.txt`
 
-### Other lists
+## Other lists
 
+* `brutas-extensions.txt` - extensions especially useful when combined with `brutas-http-paths.txt`
+* `brutas-http-params.txt` - simplistic and realistic approach to HTTP parameters
+* `brutas-http-paths.txt` - no path traversal or pseudo exploits to keep low profile, no subs (use recursion instead) - paths only
 * `brutas-usernames.txt` - most common usernames
 * `brutas-usernames-small.txt` - a short list of usernames
-* `brutas-*-tomcat.txt` - ...guess ;)
-* `brutas-extensions.txt` - extensions especially useful when combined with `brutas-http-paths.txt`
-* `brutas-http-paths.txt` - no path traversal or pseudo exploits to keep low profile, no subs (use recursion instead) - paths only
+* `brutas-usernames-tomcat.txt` - ...guess ;)
 
-### Subdomains
+## Subdomains
 
 * `brutas-subdomains-3k.txt` - a fairly reasonable list for host discovery composed of common conventions, self-hosted software etc.
 * `brutas-subdomains-250k.txt` - extended list with some extra pre-/postfixes like `host-srv`, `f.host` or `host10`
-* `brutas-subdomains-keywords.*` - keywords and rules used to generate lists for subdomains
-* `brutas-subdomains-extra.txt` - additional prefixes for subdomain discovery
+
+## Keywords
+
+* `keywords/brutas-en-common.txt` - set of most frequent English words used in passwords internationally (also from literature, pop culture etc)
+* `keywords/brutas-en-less.txt` - less frequent English words used in passwords by native speakers
+* `keywords/brutas-*` - other languages, keywords not present in English lists, based mostly on leaks
+* `keywords/brutas-subdomains.txt` - keywords and rules used to generate lists for subdomains
+* `keywords/brutas-subdomains-extra.txt` - additional prefixes for subdomain discovery
+* `keywords/brutas-wifi.txt` - bits and pieces useful in generating passwords for wireless networks
+
+## Building
+
+The build process is automated and handled by the script located in `./bin/rebuild.sh`. Check it out to understand what are the blocks and how I set the priorities (or in other words what is most probable in my opinion).
--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 echo "Preparing keyword lists..."
-sort -f brutas-passwords-keywords-en-more-common.txt > sorted1
-sort -f brutas-passwords-keywords-en-less-common.txt > sorted2
-comm -13 sorted1 sorted2 > brutas-passwords-keywords-en-less-common.txt
-rm brutas-passwords-keywords-en-more-common.txt sorted2
-mv sorted1 brutas-passwords-keywords-en-more-common.txt
+sort -f keywords/brutas-en-common.txt > sorted1
+sort -f keywords/brutas-en-less.txt > sorted2
+comm -13 sorted1 sorted2 > keywords/brutas-en-less.txt
+rm keywords/brutas-en-common.txt sorted2
+mv sorted1 keywords/brutas-en-common.txt
 
 echo "Generating sets based on usernames..."
-hashcat -r src/brutas-passwords-extra.rule --stdout brutas-usernames-small.txt | sort -f | uniq  > usernames-small-extra.tmp
-hashcat -r src/brutas-passwords-years.rule --stdout brutas-usernames-small.txt | sort -f | uniq  > usernames-small-years.tmp
-hashcat -r src/brutas-passwords-complex.rule --stdout brutas-usernames-small.txt | sort -f | uniq > usernames-small-complex.tmp
-hashcat -r src/brutas-passwords-hax0r.rule --stdout brutas-usernames-small.txt | sort -f | uniq > usernames-small-hax0r.tmp
+hashcat -r rules/brutas-passwords-extra.rule --stdout brutas-usernames-small.txt | sort -f | uniq  > usernames-small-extra.tmp
+hashcat -r rules/brutas-passwords-years.rule --stdout brutas-usernames-small.txt | sort -f | uniq  > usernames-small-years.tmp
+hashcat -r rules/brutas-passwords-complex.rule --stdout brutas-usernames-small.txt | sort -f | uniq > usernames-small-complex.tmp
+hashcat -r rules/brutas-passwords-hax0r.rule --stdout brutas-usernames-small.txt | sort -f | uniq > usernames-small-hax0r.tmp
 
 echo "Parsing keywords with extra rules..."
-hashcat -r src/brutas-passwords-years.rule --stdout brutas-passwords-keywords-en-more-common.txt | sort -f | uniq  > more-common-years.tmp
-hashcat -r src/brutas-passwords-years.rule --stdout brutas-passwords-keywords-en-less-common.txt | sort -f | uniq  > less-common-years.tmp
-hashcat -r src/brutas-passwords-hax0r.rule --stdout brutas-passwords-keywords-en-more-common.txt | sort -f | uniq > more-common-hax0r.tmp
-hashcat -r src/brutas-passwords-hax0r.rule --stdout brutas-passwords-keywords-en-less-common.txt | sort -f | uniq > less-common-hax0r.tmp
+hashcat -r rules/brutas-passwords-years.rule --stdout keywords/brutas-en-common.txt | sort -f | uniq  > more-common-years.tmp
+hashcat -r rules/brutas-passwords-years.rule --stdout keywords/brutas-en-less.txt | sort -f | uniq  > less-common-years.tmp
+hashcat -r rules/brutas-passwords-hax0r.rule --stdout keywords/brutas-en-common.txt | sort -f | uniq > more-common-hax0r.tmp
+hashcat -r rules/brutas-passwords-hax0r.rule --stdout keywords/brutas-en-less.txt | sort -f | uniq > less-common-hax0r.tmp
 
 # Change the order here:
 
@@ -32,13 +32,13 @@ cat \
     | sort -f | uniq > brutas-passwords-5k.txt
 cat \
     brutas-passwords-5k.txt \
-    brutas-passwords-keywords-en-more-common.txt \
+    keywords/brutas-en-common.txt \
     usernames-small-extra.tmp \
     | sort -f | uniq > brutas-passwords-10k.txt
 cat \
     brutas-passwords-10k.txt \
     brutas-passwords-unique.txt \
-    brutas-passwords-keywords-en-less-common.txt \
+    keywords/brutas-en-less.txt \
     brutas-passwords-numbers.txt \
     usernames-small-complex.tmp \
     usernames-small-hax0r.tmp \
@@ -56,9 +56,9 @@ echo "Cleaning up..."
 rm *.tmp
 
 echo "Generating subdomains..."
-cat brutas-subdomains-keywords.txt > brutas-subdomains-3k.txt
-cat brutas-subdomains-extra.txt >> brutas-subdomains-3k.txt
+cat keywords/brutas-subdomains.txt > brutas-subdomains-3k.txt
+cat keywords/brutas-subdomains-extra.txt >> brutas-subdomains-3k.txt
 cat brutas-subdomains-3k.txt > brutas-subdomains-250k.txt
-hashcat -r src/brutas-subdomains-keywords.rule --stdout brutas-subdomains-keywords.txt >> brutas-subdomains-250k.txt
+hashcat -r rules/brutas-subdomains.rule --stdout keywords/brutas-subdomains.txt >> brutas-subdomains-250k.txt
 
 echo "Done!"
