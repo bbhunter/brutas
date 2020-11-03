cf11c6a0d63752c129be397a733726f4da2432a9
tasooshi <tasooshi@pm.me>
2020-11-03 20:54:21 +0100

    Fixed some rules logic, others improved, better probability distribution

--- README.md
+++ README.md
@@ -4,15 +4,14 @@
 
 ## Passwords
 
-The `brutas-passwords-10k.txt` list seems to be most effective for general purpose and reasonably fast password bruteforcing, while `brutas-passwords-1k.txt` is designed for a quick win in large networks. Somewhat balanced version of these two is `brutas-passwords-5k.txt` which is perfect for targeting local network services and admin accounts thanks to close keys combinations.
+The `brutas-passwords-10k.txt` list seems to be most effective for general purpose and reasonably fast password bruteforcing, while `brutas-passwords-1k.txt` is designed for a quick win in large networks.
 
 However, I recommend experimenting on your own and rebuilding these sets depending on the target. You may want to incorporate your native language keywords, too. For example, file or a domain name combined with `brutas-passwords-numbers.txt` turns out to be pretty effective on encrypted archives and wireless networks. As with everything, a little social engineering comes handy to understand the local approach to the "password policy".
 
 * `brutas-passwords-1k.txt`
-* `brutas-passwords-5k.txt`
 * `brutas-passwords-10k.txt`
-* `brutas-passwords-50k.txt`
-* `brutas-passwords-400k.txt`
+* `brutas-passwords-60k.txt`
+* `brutas-passwords-750k.txt`
 * `brutas-passwords-classics.txt`
 * `brutas-passwords-closekeys.txt`
 * `brutas-passwords-numbers.txt`
--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -10,8 +10,9 @@ mv sorted1 keywords/brutas-en-common.txt
 echo "Generating sets based on usernames..."
 hashcat -r rules/brutas-passwords-extra.rule --stdout brutas-usernames-small.txt | sort -f | uniq  > usernames-small-extra.tmp
 hashcat -r rules/brutas-passwords-years.rule --stdout brutas-usernames-small.txt | sort -f | uniq  > usernames-small-years.tmp
-hashcat -r rules/brutas-passwords-complex.rule --stdout brutas-usernames-small.txt | sort -f | uniq > usernames-small-complex.tmp
 hashcat -r rules/brutas-passwords-hax0r.rule --stdout brutas-usernames-small.txt | sort -f | uniq > usernames-small-hax0r.tmp
+hashcat -r rules/brutas-passwords-extra.rule --stdout brutas-usernames.txt | sort -f | uniq > usernames-extra.tmp
+hashcat -r rules/brutas-passwords-hax0r.rule --stdout brutas-usernames.txt | sort -f | uniq > usernames-hax0r.tmp
 
 echo "Parsing keywords with extra rules..."
 hashcat -r rules/brutas-passwords-years.rule --stdout keywords/brutas-en-common.txt | sort -f | uniq  > more-common-years.tmp
@@ -29,28 +30,26 @@ cat \
 cat \
     brutas-passwords-1k.txt \
     brutas-passwords-closekeys.txt \
-    | sort -f | uniq > brutas-passwords-5k.txt
-cat \
-    brutas-passwords-5k.txt \
-    keywords/brutas-en-common.txt \
     usernames-small-extra.tmp \
+    usernames-small-hax0r.tmp \
     | sort -f | uniq > brutas-passwords-10k.txt
 cat \
     brutas-passwords-10k.txt \
-    brutas-passwords-unique.txt \
-    keywords/brutas-en-less.txt \
     brutas-passwords-numbers.txt \
-    usernames-small-complex.tmp \
-    usernames-small-hax0r.tmp \
-    more-common-hax0r.tmp \
-    | sort -f | uniq > brutas-passwords-50k.txt
+    brutas-passwords-unique.txt \
+    keywords/brutas-en-common.txt \
+    usernames-extra.tmp \
+    usernames-hax0r.tmp \
+    | sort -f | uniq > brutas-passwords-60k.txt
 cat \
-    brutas-passwords-50k.txt \
+    brutas-passwords-60k.txt \
+    keywords/brutas-en-less.txt \
     less-common-hax0r.tmp \
-    usernames-small-years.tmp \
-    more-common-years.tmp \
     less-common-years.tmp \
-    | sort -f | uniq > brutas-passwords-400k.txt
+    more-common-hax0r.tmp \
+    more-common-years.tmp \
+    usernames-small-years.tmp \
+    | sort -f | uniq > brutas-passwords-750k.txt
 
 echo "Cleaning up..."
 rm *.tmp
